9d62b025527b10567c51127d9e5e99a90628387c
oalapuper <akuraev780@gmail.com>
2025-09-29 11:05:35 +0300

    simple terminal with base commands

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("hello, World!")
-c = 25
+import os
+while True:
+    print(">>", end ='')
+    answ = input()
+    
+    if(answ == "exit"):
+        print("leaving from programm")
+        break
+    elif(answ == "clear"):
+        os.system("cls")
+    elif(answ == "echo"):
+        cl = input()
+        print(cl)
